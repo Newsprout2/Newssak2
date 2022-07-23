63dbfa92d790fb2b88226d5cface4654a11b5929
Newsprout2 <Newssak2@gmail.com>
2022-07-24 00:48:12 +0900

    Python Start

---
+++ practice.py
@@ -0,0 +1,36 @@
+# from math import *
+# print(floor(4.99))
+# print(ceil(3.13))
+# print(sqrt(16))
+
+#from random import *
+
+# print(random() * 10)
+# print(int(random() * 10))
+# print(int(random() * 10 + 1))
+# print(int(random() * 45) + 1)
+#print(randrange(1, 46))
+# for i in range(6):
+#     print(randint(1, 45))
+
+#date = randint(4, 28)
+
+#print('오프라인 스터디 모임 날짜는 매월 ', str(date), '일로 선정되었습니다.')
+
+
+# phonnum = "010-2233-1122"
+
+# print("앞 번호 : " + phonnum[0:3])
+# print("가운데 : " + phonnum[4:8])
+# print("끝 번호 : " + phonnum[9:])
+# print("앞 번호 : " + phonnum[:3])
+# print("전체 : " + phonnum[:])
+
+python = "Python is Amazing"
+
+index = python.index("n")
+print(index)
+index = python.index("n", index + 1)
+print(index)
+print(python.find("n"))
+print(python.count("n"))
